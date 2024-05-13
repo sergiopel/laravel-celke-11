1fbd4abdf2ee86b512cc759ee504555e52001580
Sergio Pelegrini <serpel@gmail.com>
2024-05-13 10:13:56 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Sergio</title>
+
+    </head>
+    <body>
+        <h1>Bem vindo ao meu site</h1>
+
+        {{-- <p>Data atual: {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }}</p> --}}
+    </body>
+</html>
